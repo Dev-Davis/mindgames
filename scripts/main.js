9bd9904fea634b2ad12ee6ad1a879c5dc50c69c3
Dev Davis <devdavis@Devs-MacBook-Pro.local>
2020-11-01 12:44:01 -0600

    switching to dynamic js

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,20 @@ console.log("mindgames");
 
 let array1 = ["Marquita", "Sean", "Peanut", "Jenacis", "Mikel"];
 
+console.log(array1.join());
+console.log(array1.push("A cat"));
+
 const goButton = document.getElementById("go-button");
 const slowButton = document.getElementById("slow-button");
 const stopButton = document.getElementById("stop-button");
+const addMoney = document.getElementById("getMoney");
 
-goButton.addEventListener("click", function () {
-  console.log("go button clicked");
+const playerDough = 1000;
+
+addMoney.addEventListener("click", function () {
+  console.log("get money button was clicked");
 });
 
-slowButton.addEventListener("click", function () {
-  console.log("slow button clicked");
+array1.forEach((name) => {
+  console.log(name);
 });
-stopButton.addEventListener("click", function () {
-  console.log("stop button clicked");
-});
-
-for (let i = 0; i < array1.length; i++) {
-  console.log(array1[i]);
-}
